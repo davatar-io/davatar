e1442b9c9c7831921adebc32348cf8631b18e6ea
Jack Yeh <jckyeh@gmail.com>
2022-06-25 17:33:51 -0400

    add account, remove router mistake

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['storage.googleapis.com']
-  }
-}
+    domains: ["storage.googleapis.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,16 +1,45 @@
 import type { NextPage } from "next";
-import { useRouter, withRouter } from "next/router";
-
+import { useRouter } from "next/router";
+import Image from "next/image";
 import useWallet from "hooks/useWallet";
+import NFTGallery from "components/NFTGallery";
 
-const AccountPage: NextPage = ({ router: fromEdit }) => {
-  console.log("selected NFt ", fromEdit.query.selectedNFT);
+interface CoverImageProps {
+  src: string;
+}
 
+const AccountPage: NextPage = () => {
   const { wallet } = useWallet();
   const router = useRouter();
+
+  const CoverImage = ({ src }: CoverImageProps) => {
+    return (
+      <div className="">
+        <Image
+          src={src}
+          alt="nft"
+          layout="responsive"
+          // objectFit="contain"
+          height={120}
+          width={1200}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1>{wallet?.address}</h1>
+      <CoverImage src="https://storage.googleapis.com/sentinel-nft/raw-assets/113860d69523b0ff742ef62aa51649b41dadcf4bc8e9c7ce6d37b636212bc0bd.jpeg" />
+      <div className="flex">
+        <Image
+          src="https://storage.googleapis.com/sentinel-nft/raw-assets/f741b19deee41d289b7f6f21c5f063015bb0b4df257415fa08aabde17b58673c.png"
+          alt="nft"
+          layout="fixed"
+          objectFit="contain"
+          height={120}
+          width={1200}
+        />
+      </div>
       <button
         className="btn btn-primary"
         onClick={() => {
@@ -19,8 +48,18 @@ const AccountPage: NextPage = ({ router: fromEdit }) => {
       >
         Edit My Account
       </button>
+      <h1>{wallet?.address || "Display Name"}</h1>
+      <p>
+        Bio: Web3 designer, builder, and explorer. On a mission to solve global
+        coordination problems.
+      </p>
+      <NFTGallery
+        address={
+          wallet?.address || "0x78A42a84bFE3E173C3A9246b3F5F1c5Aa8BBaE72"
+        }
+      />
     </div>
   );
 };
 
-export default withRouter(AccountPage);
+export default AccountPage;

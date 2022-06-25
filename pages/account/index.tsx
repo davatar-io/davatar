11ee7d83c66449fc1241e1a06515752526f4d8c2
Jack Yeh <jckyeh@gmail.com>
2022-06-25 17:58:52 -0400

    accounts cover gradient

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -4,41 +4,34 @@ import Image from "next/image";
 import useWallet from "hooks/useWallet";
 import NFTGallery from "components/NFTGallery";
 
-interface CoverImageProps {
-  src: string;
-}
-
 const AccountPage: NextPage = () => {
   const { wallet } = useWallet();
   const router = useRouter();
 
-  const CoverImage = ({ src }: CoverImageProps) => {
+  const CoverImage = () => {
     return (
-      <div className="">
-        <Image
-          src={src}
-          alt="nft"
-          layout="responsive"
-          // objectFit="contain"
-          height={120}
-          width={1200}
-        />
+      <div className="flex px-6 pt-6 z-0">
+        <div className="h-80 w-full rounded-2xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></div>
       </div>
     );
   };
 
   return (
     <div>
-      <CoverImage src="https://storage.googleapis.com/sentinel-nft/raw-assets/113860d69523b0ff742ef62aa51649b41dadcf4bc8e9c7ce6d37b636212bc0bd.jpeg" />
-      <div className="flex">
-        <Image
-          src="https://storage.googleapis.com/sentinel-nft/raw-assets/f741b19deee41d289b7f6f21c5f063015bb0b4df257415fa08aabde17b58673c.png"
-          alt="nft"
-          layout="fixed"
-          objectFit="contain"
-          height={120}
-          width={1200}
-        />
+      <div className="">
+        <div className="flex px-6 pt-6 z-0">
+          <div className="h-80 w-full rounded-2xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></div>
+        </div>
+        <div className="flex z-10 -mt-20 mx-auto">
+          <Image
+            src="https://storage.googleapis.com/sentinel-nft/raw-assets/f741b19deee41d289b7f6f21c5f063015bb0b4df257415fa08aabde17b58673c.png"
+            alt="nft"
+            layout="fixed"
+            objectFit="contain"
+            height={160}
+            width={160}
+          />
+        </div>
       </div>
       <button
         className="btn btn-primary"
@@ -49,10 +42,6 @@ const AccountPage: NextPage = () => {
         Edit My Account
       </button>
       <h1>{wallet?.address || "Display Name"}</h1>
-      <p>
-        Bio: Web3 designer, builder, and explorer. On a mission to solve global
-        coordination problems.
-      </p>
       <NFTGallery
         address={
           wallet?.address || "0x78A42a84bFE3E173C3A9246b3F5F1c5Aa8BBaE72"

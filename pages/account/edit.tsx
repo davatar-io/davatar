e027f1908b3f423f404efb841eb1a0e170d1fe6b
Brian Li <brian.li.101@gmail.com>
2022-06-25 11:14:05 -0700

    adding an image dropzone

--- pages/account/edit.tsx
+++ pages/account/edit.tsx
@@ -3,24 +3,68 @@ import type { NextPage } from 'next';
 import axios from 'axios';
 import Image from 'next/image';
 
+import ImageDropzone from 'components/ImageDropzone';
 import NFTGallery from 'components/NFTGallery';
+
 import useWallet from 'hooks/useWallet';
 
 const AccountEditPage: NextPage = () => {
   const { wallet } = useWallet();
   const [image, setImage] = useState<any>();
+  const [imageType, setImageType] = useState<'nft' | 'upload'>('nft');
+
+  const renderImageTypeButtonGroup = () => {
+    return (
+      <div className="btn-group">
+        <button
+          className={`btn ${imageType === 'nft' && 'btn-active'}`}
+          onClick={() => {
+            setImageType('nft');
+          }}
+        >
+          NFTs
+        </button>
+        <button
+          className={`btn ${imageType === 'upload' && 'btn-active'}`}
+          onClick={() => {
+            setImageType('upload');
+          }}
+        >
+          Upload
+        </button>
+      </div>
+    );
+  };
+
+  const renderImageSelect = () => {
+    return (
+      <div>
+        {imageType === 'nft' ? (
+          <NFTGallery
+            address={
+              wallet?.address || '0x78A42a84bFE3E173C3A9246b3F5F1c5Aa8BBaE72'
+            }
+            onSelect={(selectedNFT) => {
+              // console.log('this nft was selected', selectedNFT)
+            }}
+          />
+        ) : (
+          <ImageDropzone
+            onImageSelect={(img) => {
+              console.log(img);
+              alert('image was set');
+            }}
+          />
+        )}
+      </div>
+    );
+  };
 
   return (
     <div>
       <p>edit stuff</p>
-      <NFTGallery
-        address={
-          wallet?.address || '0x78A42a84bFE3E173C3A9246b3F5F1c5Aa8BBaE72'
-        }
-        onSelect={(selectedNFT) => {
-          // console.log('this nft was selected', selectedNFT)
-        }}
-      />
+      {renderImageTypeButtonGroup()}
+      {renderImageSelect()}
     </div>
   );
 };

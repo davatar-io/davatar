ec090c40c8e73e2dbb4b8e63ff2c692b2ff2300b
Jack Yeh <jckyeh@gmail.com>
2022-06-25 01:18:00 -0400

    wip

--- pages/[address]/index.tsx
+++ pages/[address]/index.tsx
@@ -1,16 +1,57 @@
-import { useEffect } from 'react';
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import axios from "axios";
+// import Image from "next/image";
 
 const AddressPage: NextPage = () => {
   const router = useRouter();
   const { address } = router.query;
+  const [nfts, setNfts] = useState<any>();
 
-  useEffect(() => {}, [address]);
+  useEffect(() => {
+    if (address) {
+      axios
+        .get(`https://api.nftport.xyz/v0/accounts/${address}`, {
+          params: {
+            account_address: address,
+            chain: "ethereum",
+            include: "metadata",
+            page_size: "10",
+          },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "51bb64f4-d1da-462a-b285-081b6db439fc",
+          },
+        })
+        .then((result) => {
+          console.log(result.data);
+          setNfts(result.data.nfts);
+        });
+    }
+  }, [address]);
 
   return (
     <div>
       <h1>{address}</h1>
+      <div className="flex">
+        {nfts &&
+          nfts.map((nft, i) => {
+            console.log("nft", nft);
+
+            // return (
+            //   <div key={i}>
+            //     nft
+            //     <Image
+            //       src={nft.cached_file_url}
+            //       alt="nft"
+            //       width="500"
+            //       height="500"
+            //     />
+            //   </div>
+            // );
+          })}
+      </div>
     </div>
   );
 };

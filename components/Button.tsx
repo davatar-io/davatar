c2845f7da023fc8618935dad4674c09b12afe7a3
Jack Yeh <jckyeh@gmail.com>
2022-06-25 20:56:41 -0400

    update buttons to use Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-
 interface ButtonProps {
   label: string;
-  callback: () => void;
+  tabIndex?: number;
+  onClick?: () => void;
 }
 
-const Button = ({ label, callback }: ButtonProps) => (
+const Button = ({ label, onClick, tabIndex }: ButtonProps) => (
   <button
     className={`px-6 py-3 bg-gray-900 font-semibold text-white rounded-xl hover:bg-gray-800 active:scale-105 transition-all ease-in-out`}
-    onClick={callback}
+    onClick={onClick}
+    tabIndex={tabIndex}
   >
     {label}
   </button>
--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -1,5 +1,6 @@
-import WalletManager from 'managers/WalletManager';
-import { useWallet } from 'context/WalletContext';
+import WalletManager from "managers/WalletManager";
+import { useWallet } from "context/WalletContext";
+import Button from "./Button";
 
 interface Props {}
 
@@ -8,9 +9,7 @@ const ConnectWalletButton = () => {
   if (wallet) {
     return (
       <div className="dropdown dropdown-end">
-        <button tabIndex={0} className="btn btn-primary">
-          {wallet.ens ? wallet.ens : wallet.address}
-        </button>
+        <Button tabIndex={0} label={wallet.ens ? wallet.ens : wallet.address} />
         <ul
           tabIndex={0}
           className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
@@ -28,15 +27,15 @@ const ConnectWalletButton = () => {
   }
 
   return (
-    <button
-      className="btn btn-primary"
-      tabIndex={0}
-      onClick={() => {
-        WalletManager.connect();
-      }}
-    >
-      Connect Wallet
-    </button>
+    <>
+      <Button
+        label="Connect wallet"
+        tabIndex={0}
+        onClick={() => {
+          WalletManager.connect();
+        }}
+      />
+    </>
   );
 };
 

507bc6854f9d8ac0a6e371545554644af21cc7d8
Brian Li <brian.li.101@gmail.com>
2022-06-25 23:32:44 -0700

    resolving import issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import { useWallet } from 'context/WalletContext';
 import WalletManager from 'managers/WalletManager';
